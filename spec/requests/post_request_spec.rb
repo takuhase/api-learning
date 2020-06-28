98834cba84d34347682911c8a5df6d066b2a2dd7
takuhase <hasetakunoid3228@icloud.com>
2020-06-28 22:02:16 +0900

    Add request test for /api/v1/posts

---
+++ spec/requests/post_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+# RSpec.describe "Posts", type: :request do
+describe Api::V1::PostsController, type: :request do
+  it '全てのポストを取得する' do
+    FactoryBot.create_list(:post, 10)
+
+    get '/api/v1/posts'
+    json = JSON.parse(response.body)
+
+    # リクエスト成功を表す200が返ってきたか確認する。
+    expect(response.status).to eq(200)
+
+    # 正しい数のデータが返されたか確認する。
+    expect(json['data'].length).to eq(10)
+  end
+end
